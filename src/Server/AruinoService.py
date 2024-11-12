39cf1b43cf07ff859ab135a8f8158413d3fb16d1
hameast20 <ejsflqm17@gmail.com>
2024-11-12 17:35:06 +0900

    modify AruinoService.py

--- src/Server/AruinoService.py
+++ src/Server/AruinoService.py
@@ -12,9 +12,8 @@ import time
 import threading
 import socket
 
-from_class = uic.loadUiType("/home/hdk/ws/pyqt/data/opencv.ui")[0]
-
-
+from_class = uic.loadUiType("project/iot-repo-1/src/Client/Main.ui")[0]
+tcp_read = True
 
 
 class Camera(QThread):
@@ -37,70 +36,18 @@ class WindowClass(QMainWindow, from_class):
         super().__init__()
         self.setupUi(self)
 
-        self.isCameraOn = False
-        self.camera = Camera(self)
-        self.camera.daemon = True
-        self.count = 0
+    def closeEvent(self, event):
+        global tcp_read
+        message = QMessageBox.question(self, "Question", "Are you sure you want to quit?")
+        if message == QMessageBox.Yes:
+            tcp_read = False
+            tcp_thread.join()
+            event.accept()
+        else:
+            event.ignore()
 
-        self.pixmap = QPixmap()
-        self.btn_open.clicked.connect(self.openFile)
-        self.btn_camera_onoff.clicked.connect(self.cameraOnOffClicked)
-        self.camera.update.connect(self.updateCamera)
-
-
-    def updateCamera(self):
-        # self.label.setText("Camera Running : " + str(self.count))
-        # self.count += 1
-        retval, img = self.video.read()
-
-        if retval:
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-            h,w,c = img.shape
-            qimg = QImage(img.data, w,h,w*c, QImage.Format_RGB888)
-
-            self.pixmap = self.pixmap.fromImage(qimg)
-            self.pixmap = self.pixmap.scaled(self.label.width(), self.label.height())
-
-            self.label.setPixmap(self.pixmap)
-        self.count += 1
-
-    def cameraOnOffClicked(self):
-        if self.isCameraOn == False:
-            print("Camera On")
-            self.isCameraOn = True
-            self.cameraStart()
-            self.video = cv2.VideoCapture(-1)
-
-        elif self.isCameraOn == True:
-            print("Camera off")
-            self.isCameraOn = False
-            self.cameraStop()
-            self.video.release()
-
-    def cameraStart(self):
-        self.camera.running = True
-        self.camera.start()
-
-    def cameraStop(self):
-        self.camera.running = False
-        self.count = 0
-
-    def openFile(self):
-        file = QFileDialog.getOpenFileName(filter='Image (*.*)')
-
-        img = cv2.imread(file[0])
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-        h,w,c = img.shape
-        qimg = QImage(img.data, w, h, w*c, QImage.Format_RGB888)
-
-        self.pixmap = self.pixmap.fromImage(qimg)
-        self.pixmap = self.pixmap.scaled(self.label.width(), self.label.height())
-
-        self.label.setPixmap(self.pixmap)
-
-def receiveTCPEvent(server_socket, tcp_read, myWindows):
+def receiveTCPEvent(server_socket, myWindows):
+    global tcp_read
     while tcp_read is True:
         # 클라이언트 연결 대기
         client_socket, client_address = server_socket.accept()
@@ -115,13 +62,13 @@ def receiveTCPEvent(server_socket, tcp_read, myWindows):
             # 요청 파싱
             parts = data.split("&&")
             if len(parts) != 0:
-                name = parts[0]
+                key = parts[0]
                 message = parts[1]
-                response = f"어서와! {name}"
+                response = f"어서와! {key}"
 
                 # 클라이언트 이름과 메시지 출력
-                print(f"클라이언트 이름: {name}")
-                print(f"클라이언트 메시지: {message}")
+                print(f"클라이언트 Key: {key}")
+                print(f"클라이언트 메시지: {key}")
             else:
                 response = "유효하지 않은 요청"
 
@@ -146,23 +93,26 @@ if __name__=="__main__":
     myWindows = WindowClass()
 
     # 서버 설정
-    tcp_read = True
     host = "0.0.0.0"  # 서버의 IP 주소 또는 도메인 이름
     port = 8080       # 포트 번호
 
     # 서버 소켓 생성
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # server_socket.settimeout(2)
     server_socket.bind((host, port))
     server_socket.listen(5)
 
     # 쓰레드 생성 및 시작
     tcp_thread = threading.Thread(target=receiveTCPEvent, 
-                                  args=(server_socket, tcp_read, myWindows))
+                                  args=(server_socket, myWindows))
     tcp_thread.start()
+
+    print("tcp 대기 중")
 
     # GUI 시작
     myWindows.show()
     sys.exit(app.exec_())
     print("Program End.")
+
 
 
